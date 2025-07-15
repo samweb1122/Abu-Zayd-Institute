d9fd6dc14d43b16c3256a36f818cc1ec2994d9a8
Adamu Suleiman Gbale <124920139+samweb1122@users.noreply.github.com>
2025-07-15 19:24:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -192,4 +192,13 @@ Comments: ${comments}`;
       });
     });
 
+  function toggleSection(id) {
+    const section = document.getElementById(id);
+    if (section.style.display === "none" || section.style.display === "") {
+      section.style.display = "block";
+    } else {
+      section.style.display = "none";
+    }
+  };
+
   
