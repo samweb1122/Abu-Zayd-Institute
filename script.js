f22960d4e3528c9941945b62c051d6d88e0ea408
Adamu Suleiman Gbale <124920139+samweb1122@users.noreply.github.com>
2025-07-17 14:34:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -144,3 +144,20 @@ Comments: ${comments}`;
     }, 1000);
   });
 }
+
+    function switchTab(tabId) {
+      const contents = document.querySelectorAll(".tab-content");
+      const buttons = document.querySelectorAll(".tab-btn");
+
+      contents.forEach((content) => {
+        content.classList.remove("active");
+      });
+
+      buttons.forEach((btn) => {
+        btn.classList.remove("active");
+      });
+
+      document.getElementById(tabId).classList.add("active");
+      event.target.classList.add("active");
+    }
+  
